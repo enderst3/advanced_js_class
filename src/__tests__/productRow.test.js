682e2c3e22f3a3c9564aa47ffba2ca3805760d66
enderst3 <enderst3@gmail.com>
2017-04-26 18:45:36 -0700

    more tests

--- src/__tests__/productRow.test.js
+++ src/__tests__/productRow.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ProductRow from '../ProductRow'
+import ProductRow, { makeKey } from '../ProductRow'
 import { shallow } from 'enzyme'
-/* global it describe expect jest */
+
 // import renderer from 'react-test-renderer';
 
-/* global it describe */
+/* global it describe expect jest */
 
 describe('ProductRow', () => {
   it('performs callback functionality', () => {
@@ -30,7 +30,6 @@ describe('ProductRow', () => {
 
   it('will show out of stock items in red', () => {
     const callback = jest.fn()
-
     const wrapper = shallow(
       <ProductRow
         currentCategory='electronics'
@@ -43,7 +42,6 @@ describe('ProductRow', () => {
         searchTerm=''
         onIsBuying={callback}
       />
-
     )
     const itemObject = wrapper.find('.item-color')
     expect(itemObject.node.props.style.color).toBe('red')
@@ -52,7 +50,6 @@ describe('ProductRow', () => {
 
   it('will show in stock items in black', () => {
     const callback = jest.fn()
-
     const wrapper = shallow(
       <ProductRow
         currentCategory='electronics'
@@ -65,9 +62,53 @@ describe('ProductRow', () => {
         searchTerm=''
         onIsBuying={callback}
       />
-
     )
     const itemObject = wrapper.find('.item-color')
     expect(itemObject.node.props.style.color).toBe('black')
   })
+
+  it('will not render if not in stock, and checking for stocked items', () => {
+    const callback = jest.fn()
+    const wrapper = shallow(
+      <ProductRow
+        currentCategory='electronics'
+        category='electronics'
+        name='iphone 7'
+        price={429}
+        stocked={false}
+        inStock
+        isBuying={{}}
+        searchTerm=''
+        onIsBuying={callback}
+      />
+    )
+    const itemObject = wrapper.find('input')
+    expect(itemObject.length).toBe(0)
+  })
+
+  it('will not render if not searched', () => {
+    const callback = jest.fn()
+    const wrapper = shallow(
+      <ProductRow
+        currentCategory='electronics'
+        category='electronics'
+        name='iphone 7'
+        price={429}
+        stocked
+        inStock
+        isBuying={{}}
+        searchTerm='elvis'
+        onIsBuying={callback}
+      />
+    )
+    const itemObject = wrapper.find('input')
+    expect(itemObject.length).toBe(0)
+  })
 })
+
+// describe('makeKey', () => {
+//   it('can make key', () => {
+//     let newKey = makeKey('king', 'elvis')
+//     expect(newKey).toEqual('king-elvis')
+//   })
+// })
